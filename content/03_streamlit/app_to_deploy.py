952a88d0775e7edccbe0c5e44ce9f5f98530b0e9
TomMonks <thomas.m.monks@gmail.com>
2023-10-13 15:27:23 +0100

    PEP8: foratted app_to_deploy using black

--- content/03_streamlit/app_to_deploy.py
+++ content/03_streamlit/app_to_deploy.py
@@ -1,8 +1,8 @@
-'''
+"""
 The code in this streamlit script adds in a plotly 
 chart to display a histogram of replications. The results shown in the 
 histogram can be selected by the user.
-'''
+"""
 import streamlit as st
 
 import urllib.request as request
@@ -11,11 +11,14 @@ import urllib.request as request
 import plotly.graph_objects as go
 from model import Experiment, multiple_replications
 
-INTRO_FILE = 'https://raw.githubusercontent.com/health-data-science-OR/' \
-    + 'simpy-streamlit-tutorial/main/content/03_streamlit/resources/model_info.md'
+INTRO_FILE = (
+    "https://raw.githubusercontent.com/health-data-science-OR/"
+    + "simpy-streamlit-tutorial/main/content/03_streamlit/resources/model_info.md"
+)
+
 
 def read_file_contents(path):
-    '''
+    """
     Download the content of a file from the GitHub Repo and return as a utf-8 string
 
     Notes:
@@ -33,34 +36,34 @@ def read_file_contents(path):
     --------
     utf-8 str
 
-    '''
+    """
     response = request.urlopen(path)
     return response.read().decode("utf-8")
-  
+
 
 def create_user_filtered_hist(results):
-    '''
+    """
     Create a plotly histogram that includes a drop down list that allows a user
     to select which KPI is displayed as a histogram
-    
+
     Params:
     -------
     results: pd.Dataframe
         rows = replications, cols = KPIs
-        
+
     Returns:
     -------
     plotly.figure
-    
+
     Sources:
     ------
     The code in this function was parly adapted from two sources:
     1. https://stackoverflow.com/questions/59406167/plotly-how-to-filter-a-pandas-dataframe-using-a-dropdown-menu
-    
+
     Thanks and credit to `vestland` the author of the reponse.
-    
+
     2. https://plotly.com/python/dropdowns/
-    '''
+    """
     # create a figure
     fig = go.Figure()
 
@@ -73,48 +76,58 @@ def create_user_filtered_hist(results):
     # the params in the code would need to vary depending on the type of chart.
     # The histogram will show the first KPI by default
     for col in results.columns:
-        buttons.append(dict(method='restyle',
-                            label=col,
-                            visible=True,
-                            args=[{'x':[results[col]],
-                                   'type':'histogram'}, [0]],
-                            )
-                      )
+        buttons.append(
+            dict(
+                method="restyle",
+                label=col,
+                visible=True,
+                args=[{"x": [results[col]], "type": "histogram"}, [0]],
+            )
+        )
 
     # create update menu and parameters
     updatemenu = []
     your_menu = dict()
     updatemenu.append(your_menu)
 
-    updatemenu[0]['buttons'] = buttons
-    updatemenu[0]['direction'] = 'down'
-    updatemenu[0]['showactive'] = True
-    updatemenu[0]['x'] = 0.25
-    updatemenu[0]['y'] = 1.1
-    updatemenu[0]['xanchor'] = 'right'
-    updatemenu[0]['yanchor'] = 'bottom'
-    #updatemenu[0]['pad'] = {"r": 10, "t": 10}
-    
+    updatemenu[0]["buttons"] = buttons
+    updatemenu[0]["direction"] = "down"
+    updatemenu[0]["showactive"] = True
+    updatemenu[0]["x"] = 0.25
+    updatemenu[0]["y"] = 1.1
+    updatemenu[0]["xanchor"] = "right"
+    updatemenu[0]["yanchor"] = "bottom"
+    # updatemenu[0]['pad'] = {"r": 10, "t": 10}
+
     # add dropdown menus to the figure
-    fig.update_layout(showlegend=False, 
-                      updatemenus=updatemenu)
-    
+    fig.update_layout(showlegend=False, updatemenus=updatemenu)
+
     # add label for selecting performance measure
     fig.update_layout(
-    annotations=[
-        dict(text="Performance measure", x=0, xref="paper", y=1.25, 
-             yref="paper", align="left", showarrow=False)
-    ])
+        annotations=[
+            dict(
+                text="Performance measure",
+                x=0,
+                xref="paper",
+                y=1.25,
+                yref="paper",
+                align="left",
+                showarrow=False,
+            )
+        ]
+    )
     return fig
+
+
 ##################################################################################
 
 
 #  update to wide page settings to help display results side by side
 st.set_page_config(
-     page_title="Urgent Care Sim App",
-     layout="wide",
-     initial_sidebar_state="expanded",
- )
+    page_title="Urgent Care Sim App",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 # We add in a title for our web app's page
 st.title("Urgent care call centre")
@@ -124,48 +137,46 @@ st.markdown(read_file_contents(INTRO_FILE))
 
 # side bar
 with st.sidebar:
-
     # set number of resources
-    n_operators = st.slider('Call operators', 1, 20, 13, step=1)
-    n_nurses = st.slider('Nurses', 1, 15, 9, step=1)
+    n_operators = st.slider("Call operators", 1, 20, 13, step=1)
+    n_nurses = st.slider("Nurses", 1, 15, 9, step=1)
 
     # set chance of nurse
-    chance_callback = st.slider('Chance of nurse callback', 0.1, 1.0, 0.4,
-                                step=0.05, help='Set the chance of a call back')
+    chance_callback = st.slider(
+        "Chance of nurse callback",
+        0.1,
+        1.0,
+        0.4,
+        step=0.05,
+        help="Set the chance of a call back",
+    )
 
     # set number of replications
     n_reps = st.number_input("No. of replications", 100, 1_000, step=1)
 
 # create experiment
-exp = Experiment(n_operators=n_operators, n_nurses=n_nurses,
-                 chance_callback=chance_callback)
+exp = Experiment(
+    n_operators=n_operators, n_nurses=n_nurses, chance_callback=chance_callback
+)
 
 # A user must press a streamlit button to run the model
 if st.button("Run simulation"):
-
     #  add a spinner and then display success box
-    with st.spinner('Simulating the urgent care system...'):
+    with st.spinner("Simulating the urgent care system..."):
         # run multiple replications of experment
         results = multiple_replications(exp, n_reps=n_reps)
-    
-    st.success('Done!')
+
+    st.success("Done!")
 
     col1, col2 = st.columns(2)
-    with col1.expander('Tabular results', expanded=True):
+    with col1.expander("Tabular results", expanded=True):
         # show tabular results
         st.dataframe(results.describe())
 
-    with col2.expander('Histogram', expanded=True):
-    
+    with col2.expander("Histogram", expanded=True):
         # #########################################################################
         # MODIFICATION: call updated plotly function
         fig = create_user_filtered_hist(results)
         ###########################################################################
-     
+
         st.plotly_chart(fig, use_container_width=True)
-
-
-    
-    
-    
-
